591d79b3ec06bff1fe6905197515a1adebc0e3cb
Elias Zain <ezain2t@gmail.com>
2017-07-06 15:16:05 +0200

    add unit test config and first test

--- README.md
+++ README.md
@@ -11,5 +11,5 @@ Uses
 - [x] LocalStorage
 - [x] Service Worker
 - [x] Installable Web App in Chrome for Android
-- [ ] Unit test in Angular (with TestBed)
+- [x] Unit test in Angular (with TestBed)
 - [ ] Component-based architecture (?)
---
+++ src/app/home/home.spec.ts
@@ -0,0 +1,38 @@
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {By} from '@angular/platform-browser';
+
+import {DebugElement} from '@angular/core';
+import {IonicModule} from 'ionic-angular';
+import {IonicStorageModule} from '@ionic/storage';
+
+import {HomePage} from './home';
+
+describe('HomePage', () => {
+  let de: DebugElement;
+  let comp: HomePage;
+  let fixture: ComponentFixture<HomePage>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [HomePage],
+      imports: [
+        IonicStorageModule.forRoot(),
+        IonicModule.forRoot(HomePage)
+      ]
+    });
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HomePage);
+    comp = fixture.componentInstance;
+    de = fixture.debugElement.query(By.css('ion-title'));
+  });
+
+  it('should create component', () => expect(comp).toBeDefined());
+
+  it('should have a title', () => {
+    fixture.detectChanges();
+    const title = de.nativeElement;
+    expect(title.innerText).toMatch('Ratoweb!');
+  });
+});
