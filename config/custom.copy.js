d1bbb2e668f8a72195d83ae156a6eb96fd69188a
Elias Zain <ezain2t@gmail.com>
2018-04-02 09:46:38 +0200

    extends task copy from ionic default config

--- config/custom.copy.js
+++ config/custom.copy.js
@@ -1,26 +1,8 @@
-module.exports = {
-  copyAssets: {
-    src: ['{{SRC}}/assets/**/*'],
-    dest: '{{WWW}}/assets'
-  },
-  copyIndexContent: {
-    src: ['{{SRC}}/index.html', '{{SRC}}/manifest.json'],
-    dest: '{{WWW}}'
-  },
-  copyFonts: {
-    src: ['{{ROOT}}/node_modules/ionicons/dist/fonts/**/*', '{{ROOT}}/node_modules/ionic-angular/fonts/**/*'],
-    dest: '{{WWW}}/assets/fonts'
-  },
-  copyPolyfills: {
-    src: [`{{ROOT}}/node_modules/ionic-angular/polyfills/${process.env.IONIC_POLYFILL_FILE_NAME}`],
-    dest: '{{BUILD}}'
-  },
-  copySwToolbox: {
-    src: ['{{ROOT}}/node_modules/sw-toolbox/sw-toolbox.js'],
-    dest: '{{BUILD}}'
-  },
-  copyElectron: {
-    src: ['{{SRC}}/electron.js'],
-    dest: "{{WWW}}"
-  }
+const copy = require('@ionic/app-scripts/config/copy.config');
+
+copy.copyElectron = {
+  src: ['{{SRC}}/electron.js'],
+  dest: "{{WWW}}"
 };
+
+module.exports = copy;
